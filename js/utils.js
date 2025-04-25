df24553bddc0252865dbee566a0dd4c7a07b7775
Anna Kapitanova <totoshechka19@gmail.com>
2025-04-25 15:44:45 +0300

    анимация ссылок в карточке проекта

--- js/utils.js
+++ js/utils.js
@@ -19,7 +19,7 @@ export function updateLanguage(lang) {
     Object.values(translations[lang].project).forEach((project, i) => {
       // Создаем карточку проекта
       const projectCard = document.createElement("div");
-      projectCard.className = "bg-gray-800 rounded-lg overflow-hidden";
+      projectCard.className = "bg-gray-800 rounded-lg overflow-hidden flex flex-col h-full";
       // Добавляем атрибуты AOS для анимации
       projectCard.setAttribute("data-aos", "zoom-in");
       projectCard.setAttribute("data-aos-duration", "800");
@@ -40,7 +40,7 @@ export function updateLanguage(lang) {
 
       // Создаем контейнер для текстового содержимого
       const contentDiv = document.createElement("div");
-      contentDiv.className = "p-4";
+      contentDiv.className = "p-4 flex flex-col justify-between flex-1";
 
       // Создаем заголовок
       const title = document.createElement("h3");
@@ -59,7 +59,7 @@ export function updateLanguage(lang) {
       link.href = project.url;
       link.target = "_blank";
       link.rel = "noopener noreferrer";
-      link.className = "text-blue-400 hover:underline mt-2 block";
+      link.className = "text-blue-400 hover:underline mt-2 inline-block";
       link.textContent = translations[lang]["view-project"];
       contentDiv.appendChild(link);
 
